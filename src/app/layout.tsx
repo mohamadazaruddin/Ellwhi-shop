0844465e4aecb5ee2207dafca6dbf79ff9521a33
azaruddin <m.azaruddin@ilmux.com>
2023-07-29 22:45:03 +0530

    layout edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "./styles/globals.css";
-import type { Metadata } from "next";
 import theme from "~/styles/theme";
 import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ChakraProvider theme={theme}>{children}</ChakraProvider>
       </body>
     </html>
